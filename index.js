61d983d3f4991f1e617d6a6eba5291a2af564c06
prashant_1234 <chaudharyprashant@seasia.in>
2024-05-17 16:43:17 +0530

    Added .env file

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import express from "express"
 import path from "path"
 import { fileURLToPath } from 'url';
+import dotenv from 'dotenv';
+dotenv.config()
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -13,5 +15,5 @@ app.get('/',(req, res) => {
     res.sendFile(path.join(__dirname, "public", "index.html"))
 })
 
-const PORT = 8080
-app.listen(PORT, console.log(`Server is running on port ${PORT}`))
+const port = process.env.PORT || 8080
+app.listen(port, console.log(`Server is running on port ${port}`))
